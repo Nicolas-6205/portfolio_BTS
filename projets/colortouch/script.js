7c497141e2e60bb320dca77e4e53cff8c3a5b41e
Nicolas-6205 <mehaulenicolas@gmail.com>
2026-03-02 15:26:22 +0100

    Correct typos in color generation script
    Fix typos in function and variable names for color generation.

--- projets/colortouch/script.js
+++ projets/colortouch/script.js
@@ -1,1 +1,12 @@
+const box = document.getElemetById("colorbox");
 
+function rabdomColor () {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return 'rgb(${r}, ${g}, ${b});
+}
+
+box.addEventListener("click, () => {
+    box.style.backgroundColor = randomColor ();
+});
